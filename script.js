829e0cbdae72a12d5e54a10a39fc1a072cae803f
Nguyen Truong Vo <117652864+TruongNguyenVo@users.noreply.github.com>
2023-02-22 20:45:05 +0700

    update 22/2
    done responsive 799 (iPad)

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 document.getElementById("cart").onclick = function(){
 	window.alert('Add Item To Cart');
 }
+// hàm hiển thị navbar khi click vào icon bar
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+
+if(bar){
+	bar.addEventListener('click', () => {
+		nav.classList.add('active');
+	})
+}
+//hàm đóng navbar
+const close = document.getElementById('close');
+if(close){
+	close.addEventListener('click', () => {
+		nav.classList.remove('active');
+	})
+}
